dd79a40028597d08d53565cb2c0911427561ee11
slayne <majfeuguicheteau@gmail.com>
2025-05-27 16:44:04 +0200

    hg

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const form = document.querySelector("form");
+const $firstName = document.querySelector("#name");
+const $LastName = document.querySelector("#secondname");
+const $Email = document.querySelector("#email");
+const $Password = document.querySelector("#password");
+
+const $FirstNameError = document.querySelector(".name-error");
+const $LastNameError = document.querySelector(".second-name-error");
+const $EmailError = document.querySelector(".email-error");
 
 console.log(form);
 
-form.addEventListener("submit", function () {
-  console.log("sd");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // $firstName.value.length > 0
 });
