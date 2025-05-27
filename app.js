28855b3418815b9fa281cc66766e83165bb2045a
slayne <majfeuguicheteau@gmail.com>
2025-05-27 17:29:08 +0200

    presque finis

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const form = document.querySelector("form");
 const $firstName = document.querySelector("#name");
-const $LastName = document.querySelector("#secondname");
-const $Email = document.querySelector("#email");
-const $Password = document.querySelector("#password");
+const $lastName = document.querySelector("#secondname");
+const $email = document.querySelector("#email");
+const $password = document.querySelector("#password");
 
-const $FirstNameError = document.querySelector(".name-error");
-const $LastNameError = document.querySelector(".second-name-error");
-const $EmailError = document.querySelector(".email-error");
+const $firstNameError = document.querySelector(".name-error");
+const $lastNameError = document.querySelector(".second-name-error");
+const $emailError = document.querySelector(".email-error");
+const $passwordError = document.querySelector(".password-error");
+
+console.log($email.value);
 
 console.log(form);
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+
+  if ($firstName.value === "") {
+    $firstNameError.classList.remove("hidden");
+  }
+
+  if ($lastName.value === "") {
+    $lastNameError.classList.remove("hidden");
+  }
+
+  if ($email.value === "") {
+    $emailError.classList.remove("hidden");
+  }
+
+  if ($password.value.length < 8) {
+    $passwordError.classList.remove("hidden");
+  }
 
   // $firstName.value.length > 0
 });
